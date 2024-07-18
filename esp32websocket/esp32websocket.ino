61dd866ac3f2f218ae7c5f0cc38c70826281a079
poommin2543 <poommin2543@gmail.com>
2024-07-18 11:11:05 +0700

    update websocket

--- esp32websocket/esp32websocket.ino
+++ esp32websocket/esp32websocket.ino
@@ -1,91 +1,70 @@
 #include <WiFi.h>
-
 #include <WebSocketClient.h>
 
-const char* ssid     = "ACSElab";
+const char* ssid = "ACSElab";
 const char* password = "acselab1234";
 char path[] = "/";
-char host[] = "192.168.10.130:5000";
-  
-WebSocketClient webSocketClient;
+char host[] = "192.168.10.155";
+int port = 5000;
 
-// Use WiFiClient class to create TCP connections
+WebSocketClient webSocketClient;
 WiFiClient client;
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
-
-  // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
+void connectToWiFi() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
   WiFi.begin(ssid, password);
-  
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  delay(5000);
-  
-
-  // Connect to the websocket server
-  if (client.connect("192.168.10.130", 5000)) {
-    Serial.println("Connected");
-  } else {
-    Serial.println("Connection failed.");
-    while(1) {
-      // Hang on failure
-    }
-  }
-
-  // Handshake with the server
-  webSocketClient.path = path;
-  webSocketClient.host = host;
-  if (webSocketClient.handshake(client)) {
-    Serial.println("Handshake successful");
-  } else {
-    Serial.println("Handshake failed.");
-    while(1) {
-      // Hang on failure
-    }  
-  }
-
 }
 
+void connectToWebSocket() {
+  if (client.connect(host, port)) {
+    Serial.println("Connected to server");
+    webSocketClient.path = path;
+    webSocketClient.host = host;
+    if (webSocketClient.handshake(client)) {
+      Serial.println("Handshake successful");
+    } else {
+      Serial.println("Handshake failed.");
+    }
+  } else {
+    Serial.println("Connection to server failed.");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  connectToWiFi();
+  delay(5000);
+  connectToWebSocket();
+}
 
 void loop() {
-  String data;
-
   if (client.connected()) {
-    
+    String data;
     webSocketClient.getData(data);
     if (data.length() > 0) {
       Serial.print("Received data: ");
       Serial.println(data);
     }
-   
-    data = 15;
-    
+
+    data = "15";  // Ensure data is a string
     webSocketClient.sendData(data);
-    
+
   } else {
-    Serial.println("Client disconnected.");
-    while (1) {
-      // Hang on disconnect.
-    }
+    Serial.println("Client disconnected. Reconnecting...");
+    connectToWebSocket();
   }
-  
-  // wait to fully let the client disconnect
-  delay(3000);
-  
+
+  delay(1000);
 }
